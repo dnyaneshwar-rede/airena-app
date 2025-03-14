e464531a140903ea12c0f74631246553be526c08
Dnyaneshwar  Rede <dnyaneshwarrede22@gmail.com>
2025-03-14 18:07:22 +0530

    fix build issue

--- Components/home/FoundersClub.jsx
+++ Components/home/FoundersClub.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/Components/ui/button";
 import { toast } from "sonner";
 import { Crown, Loader2 } from "lucide-react";
 import { motion } from "framer-motion";
